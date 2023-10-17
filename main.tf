8e0ae1247d9e3d7f3bc2d4942e6c27a9f2672eeb
Adrian <adrianmunoz@MacBook-Pro-2.localdomain>
2023-10-17 14:53:57 +0200

    adding default sa for the VM

--- main.tf
+++ main.tf
@@ -8,6 +8,11 @@ resource "google_storage_bucket" "statebucket" {
   public_access_prevention = "enforced"
 }
 
+resource "google_service_account" "default" {
+  account_id   = "my-custom-sa"
+  display_name = "Custom SA for VM Instance"
+}
+
 resource "google_compute_instance" "default"{
   name         = "my-instance"
   machine_type = "n2-standard-2"
