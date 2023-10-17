35f83ec5536ef319b9a5fb7c0a8acb57718c4b85
Adrian <adrianmunoz@MacBook-Pro-2.localdomain>
2023-10-17 15:21:11 +0200

    changing the region to an available one

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "google_service_account" "default" {
 resource "google_compute_instance" "default"{
   name         = "my-instance"
   machine_type = "n2-standard-2"
-  zone         = "us-central1-a"
+  zone         = "europe-north1"
 
   tags = ["foo", "bar"]
 
