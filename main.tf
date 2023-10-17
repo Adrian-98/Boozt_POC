3076a492d68ff88966a7df605cc958e7ab6addfa
Adrian <adrianmunoz@MacBook-Pro-2.localdomain>
2023-10-17 13:56:43 +0200

    adding a new bucket

--- main.tf
+++ main.tf
@@ -8,6 +8,15 @@ resource "google_storage_bucket" "statebucket" {
   public_access_prevention = "enforced"
 }
 
+resource "google_storage_bucket" "statebucket1" {
+  name          = "bucket-poc-boozt1"
+  location      = var.region
+  project       = var.project
+  storage_class = "STANDARD"
+//enforced public access prevention
+  public_access_prevention = "enforced"
+}
+
 resource "google_compute_subnetwork" "poc_subnet" {
   name          = "subnetwork-poc"
   project       = var.project
