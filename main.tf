7b71340c37b8a90b723d2933848079ad8dbba597
Adrian <adrianmunoz@MacBook-Pro-2.localdomain>
2023-10-18 15:06:48 +0200

    deleting VM, adding a new bucket

--- main.tf
+++ main.tf
@@ -8,50 +8,13 @@ resource "google_storage_bucket" "statebucket" {
   public_access_prevention = "enforced"
 }
 
-resource "google_service_account" "default" {
-  account_id   = "my-custom-sa"
-  display_name = "Custom SA for VM Instance"
-}
-
-resource "google_compute_instance" "default"{
-  name         = "my-instance"
-  machine_type = "n1-standard-2"
-  zone         = "europe-north1"
-
-  tags = ["foo", "bar"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        my_label = "value"
-      }
-    }
-  }
-    // Local SSD disk
-  scratch_disk {
-    interface = "NVME"
-  }
-
-  network_interface {
-    network = "poc-network"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-
-  metadata = {
-    foo = "bar"
-  }
-
-  metadata_startup_script = "echo hi > /test.txt"
-
-  service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.default.email
-    scopes = ["cloud-platform"]
-  }
+resource "google_storage_bucket" "statebucket" {
+  name          = "bucket-poc-boozt"
+  location      = var.region
+  project       = var.project
+  storage_class = "STANDARD"
+//enforced public access prevention
+  public_access_prevention = "inherited"
 }
 
 resource "google_compute_subnetwork" "poc_subnet" {
